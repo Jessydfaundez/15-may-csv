dfdb9e18903f077313f139a07eb701f01ab6b639
Jessydfaundez <jessydfaundez@gmail.com>
2017-05-15 12:18:20 -0400

    Update about.php

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ array_walk($datos, function(&$a) use ($datos) {$a = array_combine($datos[0], $a)
 array_shift($datos);
 ?>
 <h2>Sobre el proyecto</h2>
-<p>Acá peguen sus about</p>
+<p>Esto parte desde los viajes familiares que he tenido a Combarbalá y de lo aburrido que me pareció estar en ese lugar al no encontrar turismo en la zona, la primera vez que viajé nos recorrimos el pueblo en el día y ya no había más que hacer. Tienen actividades que se pueden potenciar aún más, como el observatorio o los petroglifos, además de crear otras para dar a conocer este pueblo, hacerlo más “divertido”.</p>
 
 </section>
 <?php include('pie.php');?>
